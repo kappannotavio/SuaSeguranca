acac4f60808dac79b0c7b3f0bade369df5691912
Otávio Gonçalves Kappann dos Santos <kappannotavio@gmail.com>
2025-11-06 19:35:19 -0300

    Working Search Bar

--- static/app.js
+++ static/app.js
@@ -1,1 +1,17 @@
-console.log("Meu pau no teu ouvido");
+  document.getElementById("searchForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let input = document.getElementById("searchInput").value.toLowerCase();
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+      let title = card.querySelector(".card-title").innerText.toLowerCase();
+      let text = card.querySelector(".card-text").innerText.toLowerCase();
+
+      if (title.includes(input) || text.includes(input)) {
+        card.parentElement.style.display = "";
+      } else {
+        card.parentElement.style.display = "none";
+      }
+    });
+  });
